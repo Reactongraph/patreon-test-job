fe2761643f1c2b921c7f27200d28be56ee2f1d16
ReactOngraph <js.developer@ongraph.com>
2023-08-18 21:27:37 +0530

    profile api update

--- src/components/OAuthPage.tsx
+++ src/components/OAuthPage.tsx
@@ -9,12 +9,13 @@ const OAuthPage: React.FC = () => {
   const clientId = process.env.REACT_APP_CLIENT_ID;
   const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
   const redirectionUri = process.env.REACT_APP_REDIRECT_URL;
-  const [isLoading, setIsLoading] = useState(true);
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const [patreonOk, setPatreonOk] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      if (code) {
+      if (code && clientId && clientSecret && redirectionUri) {
         try {
           const response = await axios.post(
             `https://www.patreon.com/api/oauth2/token`,
@@ -27,6 +28,28 @@ const OAuthPage: React.FC = () => {
           );
           console.log("API Response:", response.data);
           if (response.data && response.data.access_token) {
+            const ptResponse = await fetch(
+              "https://www.patreon.com/api/oauth2/v2/identity?include=memberships.campaign&fields%5Bmember%5D=patron_status",
+              {
+                method: "GET",
+                headers: {
+                  "Access-Control-Allow-Headers" : "Content-Type",
+                  "Access-Control-Allow-Origin": "*",
+                  "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+                  accept: "application/json",
+                  Authorization: `Bearer ${response.data.access_token}`,
+                },
+              }
+            );
+
+            if (ptResponse.status !== 200) return ptResponse;
+
+            try {
+              const ptData = await ptResponse.json();
+              console.log("ptData", ptData);
+            } catch (error) {
+              console.error("Error parsing Patreon data:", error);
+            }
             setLoggedIn(true);
           }
         } catch (error) {
@@ -38,7 +61,7 @@ const OAuthPage: React.FC = () => {
     };
 
     fetchData();
-  }, [code]);
+  }, [code, clientId, clientSecret, redirectionUri]);
 
   return (
     <div
@@ -52,7 +75,13 @@ const OAuthPage: React.FC = () => {
       {isLoading ? (
         <p>Loading...</p>
       ) : (
-        <p>{loggedIn ? "Logged In" : "Not Logged In"}</p>
+        <p>
+          {loggedIn
+            ? patreonOk
+              ? "Logged In (Patreon)"
+              : "Logged In (Not Patreon)"
+            : "Not Logged In"}
+        </p>
       )}
     </div>
   );
